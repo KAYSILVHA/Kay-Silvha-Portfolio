c9b8f9bd74ec4a4d0629251cb87261d081ca0dc6
KAYSILVHA <kaysilvha@gmail.com>
2024-12-04 21:18:49 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import "./assets/style/footer-sass.scss"
 
+import Logo from "/src/Components/Header/assets/images/Logo.png";
+
 import {
-    Col
+    Col,
+    Nav,
+    NavItem,
+    NavLink,
+    Row
 } from "reactstrap";
 
 function Footer() {
@@ -11,12 +17,47 @@ function Footer() {
         <>
             <section className='container-fluid footer h-auto'>
                 <section className="container">
-                    <div className='w-100 d-flex justify-content-center align-items-center'>
-                        <Col className='col-lg-6'>
-                            <h2 className='text-light'>Let’s make something amazing together.</h2>
+                    <Row className='d-flex justify-content-between align-items-start container-footer'>
+                        <Col className='col-lg-6 col-12'>
+                            <h2 className='text-light text'>Let’s make something amazing together.</h2>
                             <h2 className='text-light'>Start by saying “<span>hey there!</span>”</h2>
                         </Col>
-                    </div>
+                        <Col className='col-lg-4 col-12 informations'>
+                            <p>Information</p>
+                            <div className="address">
+                                <h5>Paranavai Parana, Brazil</h5>
+                                <h2 className='text-white'>kaysilvha@gmail.com</h2>
+                            </div>
+                            <div className="footer-menu">
+                                <Nav className='d-flex flex-column'>
+                                    <NavItem>
+                                        <NavLink className='px-0 selected' href="#">&lt; Services &gt;</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className='p-style' href="#"> Certifications </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className='p-style' href="#"> Works </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className='p-style' href="#"> Projects </NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink className='p-style' href="#"> Contacts </NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className='container-last d-flex'>
+                        <Col className='d-flex align-items-end'>
+                            <img src={Logo} alt="Logo" />
+                            <p className='mb-0'>&#169; 2024. All Rights Reserved</p>
+                        </Col>
+                        <Col>
+                            
+                        </Col>
+                    </Row>
                 </section>
             </section>
         </>
